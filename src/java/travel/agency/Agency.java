3b21a934e9c583103f9069d9cf278f6becf45c49
Jam Timbol <jam.timbol00@gmail.com>
2023-07-27 15:41:12 +0200

    agency

--- src/java/travel/agency/Agency.java
+++ src/java/travel/agency/Agency.java
@@ -1,4 +1,37 @@
 package java.travel.agency;
 
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class Agency {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("--- WELCOME TO JAVA TRAVEL AGENCY ---");
+
+        boolean travel = true;
+        while (travel) {
+            try {
+                System.out.println("Wanna go on a holiday? Y/N: ");
+                String yn = scan.nextLine();
+
+            if (yn.equalsIgnoreCase("n")){
+                travel = false;
+            } else if (yn.equalsIgnoreCase("y"));
+                System.out.print("Insert Destination: ");
+                String destination = scan.nextLine();
+
+                System.out.print("Date departure (yyyy-MM-dd)");
+                String departureDate = scan.nextLine();
+                LocalDate departure = LocalDate.parse(departureDate);
+
+                System.out.print("Date Arrival (yyyy-MM-dd)");
+                String arrivalDate = scan.nextLine();
+                LocalDate arrival = LocalDate.parse(arrivalDate);
+
+                Holiday vacay = new Holiday(destination,departure,arrival);
+                System.out.println("Holiday confirmed! ");
+            }
+        }
+    scan.close();
+    }
 }
